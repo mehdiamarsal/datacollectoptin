80cd9e089f9b294167fa7e7a113a2f516e0e1755
mehdiamarsal <50869422+mehdiamarsal@users.noreply.github.com>
2024-10-16 13:10:25 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 const fs = require('fs');
-const keyFile  = fs.readFileSync('/home/datacollectoptin/www/api.datacollectoptin.com/key.pem');
-const certFile = fs.readFileSync('/home/datacollectoptin/www/api.datacollectoptin.com/csr.pem');
+const keyFile = fs.readFileSync('/home/datacollectoptin/ssl/keys/8e811_66a9b_3875f1901f83f16654a169248b1b83e2.key');
+const certFile = fs.readFileSync('/home/datacollectoptin/ssl/certs/api_datacollectoptin_com_8e811_66a9b_1760608687_a8dffefe6921360bcb3fb33ae21ad8d6.crt');
+
 
 //console.log(certFile)
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule,{ 
+  const app = await NestFactory.create(AppModule, { 
     httpsOptions: {
-    key: keyFile,
-    cert: certFile,
-  }});
+      key: keyFile,
+      cert: certFile,
+    }
+  });
+
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,  // Supprime les propriétés non spécifiées dans le DTO
     forbidNonWhitelisted: true,  // Renvoie une erreur si des propriétés non spécifiées sont présentes
